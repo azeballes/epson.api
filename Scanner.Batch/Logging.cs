dc1edfb3275be66e1b6aff08273fa2be4d984290
gscorp\az22207 <ariel.zeballes@supervielle.com.ar>
2019-04-28 10:54:17 -0300

    Bucle principal de proceso

---
+++ Scanner.Batch/Logging.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Scanner.Batch
+{
+    class Logging
+    {
+        static Logging() => XmlConfigurator.Configure();
+
+        public static ILog Log(Type type) => LogManager.GetLogger(type);
+
+    }
+}
---
+++ Scanner.Batch/Program.cs
@@ -0,0 +1,94 @@
+﻿using log4net;
+using Scanner.Driver;
+using System;
+using System.Threading;
+
+namespace Scanner.Batch
+{
+    class Program
+    {
+        static readonly int Delay = 5000;
+        static readonly ILog Log = Logging.Log(typeof(Program));
+        static void Main(string[] args)
+        {
+            Log.Info("Iniciando la digitalización presione <CTRL + C> para finalizar...");
+            Thread.Sleep(Delay);
+            while (true)
+            {
+                var scanner = Connect();
+                ReadDocuments(scanner);
+                Disconnect(scanner);
+            }
+        }
+
+        private static void Disconnect(IDigitalizer scanner)
+        {
+            try
+            {
+                Log.Info("Desconectando la digitalizadora...");
+                scanner.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                Log.Warn(ex.Message);
+            }
+        }
+
+        private static void ReadDocuments(IDigitalizer scanner)
+        {
+            while (true)
+            {
+                try
+                {
+                    foreach (var document in scanner.Documents)
+                    {
+                        ProcessDocument(document, scanner);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Log.Error(ex.Message);
+                    break;
+                }
+                Thread.Sleep(Delay);
+            }
+        }
+
+        private static void ProcessDocument(Document document, IDigitalizer scanner)
+        {
+            while (true)
+            {
+                try
+                {
+                    scanner.Documents.Remove(document);
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Log.Error($"No se puede guardar el docomento #{document.Id}: {ex.Message}");
+                }
+                Thread.Sleep(Delay);
+            }
+        }
+
+        private static IDigitalizer Connect()
+        {
+            while (true)
+            {
+                try
+                {
+                    Log.Info("Conectando la digitalizadora...");
+                    IDigitalizer scanner = new EpsonDigitalizer();
+                    scanner.Connect();
+                    Log.Info("Conección OK");
+                    return scanner;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.Message);
+                }
+                Thread.Sleep(Delay);
+            }
+        }
+    }
+}
